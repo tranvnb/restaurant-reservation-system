ec23062abf7f6f85b6de68d29a6d2ed5620c0b39
Gam <trant48@student.douglascollege.ca>
2021-03-27 08:55:32 -0700

    do CURD operation on table

--- src/main/java/com/csis3275/group4/repository/TableRepository.java
+++ src/main/java/com/csis3275/group4/repository/TableRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.csis3275.group4.entity.Table;
 
-public interface TableRepository extends MongoRepository<Table, Integer>{
+public interface TableRepository extends MongoRepository<Table, String>{
 
 }
